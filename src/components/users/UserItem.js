ed86c7b3824a38655628060713b5f8bf7c17ed68
Lawrence <locaulker@gmail.com>
2020-08-03 06:52:36 -0400

    added class components, generated lists, key props and map function

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,33 +1,24 @@
-import React, { Component } from "react"
+import React from "react"
 
-class UserItem extends Component {
-  // state = {
-  //   id: "id",
-  //   login: "mpjombo",
-  //   avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-  //   html_url: "https://github.com/mojombo"
-  // }
+const UserItem = props => {
+  const { avatar_url, login, html_url } = props.user
 
-  render() {
-    const { avatar_url, login, html_url } = this.props.user
-
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default UserItem
