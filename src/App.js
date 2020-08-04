786b913f38302ba7dc99f7d539a87ecea5e6a6a2
Lawrence <locaulker@gmail.com>
2020-08-04 00:42:07 -0400

    added environment variables

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ import "./App.css"
 class App extends Component {
   state = {
     users: [],
-    loading: false,
+    loading: false
   }
 
   async componentDidMount() {
     this.setState({
-      loading: true,
+      loading: true
     })
 
-    const res = await axios.get("https://api.github.com/users")
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
 
-    console.log(res.data)
     this.setState({
       users: res.data,
-      loading: false,
+      loading: false
     })
   }
 
