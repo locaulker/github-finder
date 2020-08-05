2887c0db3581a36cb8936bd6c849087db8522e98
Lawrence <locaulker@gmail.com>
2020-08-05 02:15:06 -0400

    complted search and alert functionalities

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react"
 import Navbar from "./components/layout/Navbar"
 import Users from "./components/users/Users"
 import Search from "./components/users/Search"
+import Alert from "./components/layout/Alert"
+
 import axios from "axios"
 
 import "./App.css"
@@ -10,6 +12,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null
   }
 
   // Search ALL Users
@@ -27,8 +30,8 @@ class App extends Component {
   //     loading: false,
   //   })
   // }
-
-  // Search Github Users
+  //
+  // Search Github Users  ===============
   searchUsers = async text => {
     this.setState({ loading: true })
 
@@ -39,16 +42,39 @@ class App extends Component {
     this.setState({
       users: res.data.items,
       loading: false,
+      alert: null
     })
   }
 
+  // Clear Users from state ===============
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false
+    })
+  }
+
+  // setAlert ========================
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } })
+
+    setTimeout(() => this.setState({ alert: null }), 5000)
+  }
+
   render() {
+    const { users, loading } = this.state
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     )
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -3,22 +3,32 @@ import PropTypes from "prop-types"
 
 class Search extends Component {
   state = {
-    text: "",
+    text: ""
   }
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
   }
 
   onSubmit = e => {
     e.preventDefault()
-    this.props.searchUsers(this.state.text)
-    this.setState({ text: "" })
+
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter search term", "light")
+    } else {
+      this.props.searchUsers(this.state.text)
+      this.setState({ text: "" })
+    }
   }
 
   onChange = e => this.setState({ [e.target.name]: e.target.value })
 
   render() {
+    const { showClear, clearUsers } = this.props
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -35,6 +45,11 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     )
   }
