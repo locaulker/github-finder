a648ddcc119c000c22dec2ee9590ab266142f9ff
Lawrence <Caulker>
2020-08-08 08:56:36 -0400

    added home and NotFound pages

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 style={{ color: 'orangered', fontWeight: 'normal' }}>
+        Oops! Not Found
+      </h1>
+      <p>Oops! The page you ar looking for does not exist...</p>
+    </div>
+  )
+}
+
+export default NotFound
