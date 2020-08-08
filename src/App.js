5382914845c7da32b181e59cb3d8c2738ae338a6
Lawrence <Caulker>
2020-08-08 00:59:28 -0400

    moved user and userRepos to context

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Users from './components/users/Users'
 import User from './components/users/User'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
-import axios from 'axios'
 
 import GithubState from './context/github/GithubState'
 
@@ -13,53 +12,7 @@ import './App.css'
 import About from './components/pages/About'
 
 const App = () => {
-  const [users, setUsers] = useState([])
-  const [user, setUser] = useState({})
-  const [repos, setRepos] = useState([])
-  const [loading, setLoading] = useState(false)
   const [alert, setAlert] = useState(null)
-
-  // Search Github Users  ===============
-  // const searchUsers = async text => {
-  //   setLoading(true)
-
-  //   const res = await axios.get(
-  //     `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   )
-
-  //   setUsers(res.data.items)
-  //   setLoading(false)
-  // }
-
-  // Single User Component ================
-  const getUser = async username => {
-    setLoading(true)
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    )
-
-    setUser(res.data)
-    setLoading(false)
-  }
-
-  // Get User Repos
-  const getUserRepos = async username => {
-    setLoading(true)
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    )
-
-    setRepos(res.data)
-    setLoading(false)
-  }
-
-  // Clear Users from state ===============
-  const clearUsers = () => {
-    setUsers([])
-    setLoading(false)
-  }
 
   // setAlert ========================
   const showAlert = (msg, type) => {
@@ -80,30 +33,13 @@ const App = () => {
                 path='/'
                 render={props => (
                   <Fragment>
-                    <Search
-                      clearUsers={clearUsers}
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={showAlert}
-                    />
-                    <Users loading={loading} users={users} />
+                    <Search setAlert={showAlert} />
+                    <Users />
                   </Fragment>
                 )}
               />
               <Route exact path='/about' component={About} />
-              <Route
-                exact
-                path='/user/:login'
-                render={props => (
-                  <User
-                    {...props}
-                    getUser={getUser}
-                    getUserRepos={getUserRepos}
-                    user={user}
-                    repos={repos}
-                    loading={loading}
-                  />
-                )}
-              />
+              <Route exact path='/user/:login' component={User} />
             </Switch>
           </div>
         </div>
